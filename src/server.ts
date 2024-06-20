a5097496c6cc4497f4741d93bfe49f16a3265087
Maxence <maxence.arcelin@viacesi.fr>
2024-06-20 17:44:23 +0200

    Created Dockerfile & Deleted cors

--- src/server.ts
+++ src/server.ts
@@ -10,12 +10,12 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 
-app.use(cors({
+/* app.use(cors({
     origin: 'http://localhost:4000',
     methods: ['GET', 'POST'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true
-}));
+})); */
 
 let port: number = parseInt(process.env.PORT || '5001', 10);
 
